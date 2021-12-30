f8a46dc4d64770c07ea35acd4a68cfd7726567bb
Nic Deane <njdeane7@gmail.com>
2021-12-30 10:30:51 +1000

    Initial Commit

---
+++ HelloCoreData/HelloCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreDataApp.swift
+//  HelloCoreData
+//
+//  Created by Nic Deane on 30/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
